09b1470c5bafb88d3e9137b55c6e6ad4da6a60d5
sathis kumar M <you@example.com>
2021-03-17 12:36:50 +0530

    redux saga article

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Users from './components/Userscomponent';
+
+
+function App() {
+  return (
+    <div className="App">
+           <Users />
+
+    </div>
+  );
+}
+
+export default App;
